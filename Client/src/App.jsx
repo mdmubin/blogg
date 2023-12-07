a1a386b05c6b4569641bfedf45554c9e6a96c412
mdmubin <md.mubin030@gmail.com>
2023-12-07 10:37:11 +0600

    fixed app router and redux store provider

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import store from './store/index';
 import * as Views from './views';
 
 const router = createBrowserRouter([
   { path: '/settings', element: <Views.ProfileView /> },
   { path: '/login', element: <Views.LoginView /> },
   { path: '/register', element: <Views.RegisterView /> },
+  { path: '/blog/:id', element: <Views.BlogView /> },
 ]);
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   );
 }
 
