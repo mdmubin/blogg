fc3d263de134912a3339d065c0d751a0419283a4
mdmubin <md.mubin030@gmail.com>
2023-12-25 07:58:57 +0600

    fixed profile view page

--- Client/src/redux/api/bloggApi.js
+++ Client/src/redux/api/bloggApi.js
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const authInfo = JSON.parse(localStorage.getItem('authInfo'));
+const authInfo = JSON.parse(JSON.parse(localStorage.getItem('authInfo')));
 
 const bloggApi = createApi({
   reducerPath: 'bloggApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:5268/api/',
     headers: {
-      Authorization: `Bearer ${authInfo ? authInfo.token : ''}`,
+      Authorization: `Bearer ${authInfo?.token ?? ''}`,
     },
     responseHandler: 'text',
   }),
--- Client/src/views/ProfileView.jsx
+++ Client/src/views/ProfileView.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import Types from 'prop-types';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useProfileQuery } from '../redux/api/userEndpoints';
 import Navbar from './components/Navbar';
 
-function ProfileView({ userData }) {
+function ProfileView() {
+  const navigate = useNavigate();
+
+  const { loggedIn } = useSelector((state) => state.user);
+
+  const authInfo = JSON.parse(JSON.parse(localStorage.getItem('authInfo')));
+  const {
+    isLoading, data, error, isError,
+  } = useProfileQuery(authInfo?.username);
+  const userData = isLoading || isError ? {} : JSON.parse(data);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate('/login');
+    }
+  }, [loggedIn]);
+
   const labelStyle = { width: 124 };
 
   return (
@@ -16,58 +34,68 @@ function ProfileView({ userData }) {
           </div>
 
           <div className="px-4 pb-3">
-            <img src={userData.profilePictureUrl} className="img-thumbnail" alt="pfp" width={150} height={150} />
+            <img src={userData.profilePictureUrl ?? 'https://avatars.githubusercontent.com/u/72853968?v=4'} className="img-thumbnail" alt="pfp" width={150} height={150} />
           </div>
 
           <div className="input-group px-4 mb-3">
             <span className="input-group-text" style={labelStyle}>User ID</span>
-            <input type="text" disabled aria-label="Username" className="form-control" value={userData.id} />
+            <input type="text" aria-label="Username" className="form-control" value={userData.id ?? ''} disabled />
           </div>
 
-          <form onSubmit={() => { }}>
+          <form className="pb-3">
 
             <div className="input-group px-4 mb-3">
               <span className="input-group-text" style={labelStyle}>Username</span>
-              <input type="text" aria-label="Username" className="form-control" value={userData.username} />
+              <input
+                type="text"
+                aria-label="Username"
+                className="form-control"
+                value={userData.username ?? ''}
+                disabled
+              />
             </div>
 
             <div className="input-group px-4 mb-3">
               <span className="input-group-text" style={labelStyle}>Email</span>
-              <input type="text" aria-label="Email" className="form-control" value={userData.email} />
+              <input
+                type="text"
+                aria-label="Email"
+                className="form-control"
+                value={userData.email ?? ''}
+                disabled
+              />
             </div>
 
             <div className="input-group px-4 mb-3">
               <span className="input-group-text" style={labelStyle}>Profile Picture</span>
-              <input type="text" aria-label="First name" className="form-control" value={userData.profilePictureUrl} />
+              <input
+                type="text"
+                aria-label="First name"
+                className="form-control"
+                value={userData.profilePictureUrl ?? 'https://avatars.githubusercontent.com/u/72853968?v=4'}
+                disabled
+              />
             </div>
 
-            <div className="container text-center pb-3">
-              <button type="submit" className="my-3 btn btn-primary" disabled={false}>Save Changes</button>
-            </div>
+            {/* <div className="container text-center pb-3">
+              <button type="submit" className="my-3 btn btn-primary">Save Changes</button>
+            </div> */}
 
           </form>
         </div>
       </div>
+
+      {isError
+        && (
+          <div className="d-flex justify-content-center pt-3">
+            <div className="alert alert-danger" role="alert">
+              {`Error ${error.status}: Failed to retrieve data. Try refreshing / logging back in.`}
+            </div>
+          </div>
+        )}
+
     </>
   );
 }
-
-ProfileView.propTypes = {
-  userData: Types.shape({
-    id: Types.string,
-    username: Types.string,
-    email: Types.string,
-    profilePictureUrl: Types.string,
-  }),
-};
-
-ProfileView.defaultProps = {
-  userData: {
-    id: '00000000-0000-0000-0000-000000000000',
-    username: '',
-    email: '',
-    profilePictureUrl: 'https://avatars.githubusercontent.com/u/72853968?v=4',
-  },
-};
 
 export default ProfileView;
